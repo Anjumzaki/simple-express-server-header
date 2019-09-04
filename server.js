35001182b5ad0ebbb2bd1cb1a6cec0e1d48c49b9
AnjumZaki <anjumzaki8@gmail.com>
2019-09-04 21:06:50 +0500

    added headers

--- server.js
+++ server.js
@@ -5,25 +5,24 @@ mongoose.pluralize(null);
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-mongoose.connect('mongodb://anjumzaki:anjuzmaki123@ds247827.mlab.com:47827/credits');
+mongoose.connect('mongodb://anjumzaki:anjumzaki123@ds263436.mlab.com:63436/practiceapp');
 var db = mongoose.connection;
 db.once('open', function () { });
 var key = 123;
 
 app.use(function (req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    if(req.headers.key="abc123"){
-    next();
+    if (req.headers.key == "123") {
+        next();
     }
-    else{
+    else {
         res.status(404).send({
             success: 'false',
             message: 'key is not correct',
-            article,
-        })
+                })
     }
-    });
+});
 var articleSchema = new mongoose.Schema({
     title: String,
     author: String,
@@ -37,8 +36,6 @@ var articleSchema = new mongoose.Schema({
 var Article = mongoose.model('ArticleData', articleSchema);
 
 app.post('/api/add', async (req, res) => {
-
-    
     const article = new Article({
         title: req.body.title,
         author: req.body.author,
@@ -47,44 +44,57 @@ app.post('/api/add', async (req, res) => {
 
     });
     article.save(function (err) {
-            if (err) return console.error(err);
-        });
-        res.status(200).send({
-            success: 'true',
-            message: 'article Created',
-            article,
-        })
-    }); 
+        if (err) return console.error(err);
+    });
+    res.status(200).send({
+        success: 'true',
+        message: req.headers.key,
+        article,
+
+    })
+});
+
+
+
 
 app.get("/api/get", async (req, res) => {
+
     var result = await Article.find().exec();
     res.status(200).send({
         success: 'true',
-        message: 'article Created',
+        message: req.headers.key,
+        result,
+    })
+
+    res.status(401).send({
+        success: 'false',
+        message: req.headers.key,
         result,
     })
 });
 
-app.get("/api/edit/:articleId", async(req,res) =>{
-    Article.findByIdAndUpdate(req.params.articleId, { 
-        $set: { title: req.body.title, author: req.body.author, data: req.body.data, author: req.body.genre }}, {upsert:true}, function (err, user) {
-            res.status(200).send({
-                success: 'true',
-                message: 'article Created',
-                user,
-            })
+app.get("/api/edit/:articleId", async (req, res) => {
+    Article.findByIdAndUpdate(req.params.articleId, {
+        $set: { title: req.body.title, author: req.body.author, data: req.body.data, author: req.body.genre }
+    }, { upsert: true }, function (err, user) {
+        res.status(200).send({
+            success: 'true',
+            message: 'article Created',
+            user,
+        })
     });
+});
+app.post("/api/edit/:articleId", async (req, res) => {
+    Article.findByIdAndUpdate(req.params.articleId, {
+        $set: { title: req.body.title, author: req.body.author, data: req.body.data, author: req.body.genre }
+    }, { upsert: true }, function (err, user) {
+        res.status(200).send({
+            success: 'true',
+            message: 'article Created',
+            user,
+        })
     });
-    app.post("/api/edit/:articleId", async(req,res) =>{
-        Article.findByIdAndUpdate(req.params.articleId, { 
-            $set: { title: req.body.title, author: req.body.author, data: req.body.data, author: req.body.genre }}, {upsert:true}, function (err, user) {
-                res.status(200).send({
-                    success: 'true',
-                    message: 'article Created',
-                    user,
-                })
-        });
-    });
+});
 
 const PORT = 2000;
 app.listen(PORT, () => {
